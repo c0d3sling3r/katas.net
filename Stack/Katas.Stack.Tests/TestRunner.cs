7b93b6a80143c754e1184e006627c795e152f415
Mojtaba Shojajou <shojajou@gmail.com>
2022-03-08 16:42:16 +0330

    add Stack

---
+++ Stack/Katas.Stack.Tests/TestRunner.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+
+namespace Katas.Stack.Tests;
+
+public class TestRunner
+{
+    private readonly Stack _stack;
+    
+    public TestRunner()
+    {
+        _stack = new Stack();
+    }
+    
+    [Fact]
+    public void CreateStack_ShouldBeEmpty()
+    {
+        // Assertion
+        Assert.True(_stack.IsEmpty);
+    }
+
+    [Fact]
+    public void PushToEmptyStack_SizeMustChangedToOne()
+    {
+        // Action
+        _stack.Push("first");
+        
+        // Assertion
+        Assert.Equal(1, _stack.Size);
+    }
+
+    [Fact]
+    public void PopFromOneSizeStack_SizeMustChangedToZero()
+    {
+        // Arrange
+        _stack.Push("first");
+        
+        // Action
+        object poppedObject = _stack.Pop();
+        
+        // Assertion
+        Assert.Equal("first", poppedObject);
+        Assert.Equal(0, _stack.Size);
+    }
+
+    [Fact]
+    public void Peek_MustReturnTheLastOneButBNotRemovingIt()
+    {
+        // Arrange
+        _stack.Push("first");
+        _stack.Push("second");
+        _stack.Push("third");
+        
+        // Action
+        object peekedOne = _stack.Peek();
+        
+        // Assertion
+        Assert.Equal("third", peekedOne);
+        Assert.Equal(3, _stack.Size);
+    }
+
+    [Fact]
+    public void PushOncePopTwice_ExceptionMustBeThrown()
+    {
+        // Action
+        _stack.Push("first");
+
+        Action poppingAction = () =>
+        {
+            object _ = _stack.Pop();
+            object __ = _stack.Pop();
+        };
+
+        // Assertion
+        Assert.Throws<IndexOutOfRangeException>(poppingAction);
+    }
+}
---
+++ Stack/Katas.Stack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+
+namespace Katas.Stack;
+
+public class Stack
+{
+    private object[] _elements;
+
+    public bool IsEmpty => Size == 0;
+
+    public int Size { get; private set; }
+
+    public Stack()
+    {
+        _elements = new object[0];
+    }
+
+    public void Push(object element)
+    {
+        Array.Resize(ref _elements, ++Size);
+        _elements[Size - 1] = element;
+    }
+
+    public object Pop()
+    {
+        int popElementIndex = --Size;
+        object element = _elements[popElementIndex];
+
+        object[] newElements = new object[Size];
+        
+        for (int i = 0; i < Size; i++)
+        {
+            newElements[i] = _elements[i];
+        }
+
+        _elements = newElements;
+
+        return element;
+    }
+
+    public object Peek()
+    {
+        int lastElementIndex = Size - 1;
+        object element = _elements[lastElementIndex];
+
+        return element;
+    }
+}
