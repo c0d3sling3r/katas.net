aa1ae8b37324ca92beb3319c354df4d4af9cb455
Mojtaba Shojajou <shojajou@gmail.com>
2022-03-09 16:27:04 +0330

    add Enqueue and Dequeue method for queue and its tests

--- Katas.Queue.Tests/TestRunner.cs
+++ Katas.Queue.Tests/TestRunner.cs
@@ -1,6 +1,56 @@
-﻿namespace Katas.Queue.Tests;
+﻿using Xunit;
+
+namespace Katas.Queue.Tests;
 
 public class TestRunner
 {
-    
+    private CustomQueue<string> _queue;
+
+    public TestRunner()
+    {
+        _queue = new CustomQueue<string>();
+    }
+
+    [Fact]
+    public void CreateQueue_MustBeEmpty()
+    {
+        // Arrange
+
+        // Action
+        _queue = new CustomQueue<string>();
+
+        // Assertion
+        Assert.True(_queue.IsEmpty);
+    }
+
+    [Fact]
+    public void Enqueue_SizeMustBeIncrementedByOne()
+    {
+        // Arrange
+        int beforeSize = _queue.Size;
+
+        // Action
+        _queue.Enqueue("A");
+
+        // Assertion
+        Assert.Equal(beforeSize + 1, _queue.Size);
+    }
+
+    [Theory]
+    [InlineData("A")]
+    [InlineData("B")]
+    [InlineData("C")]
+    public void Dequeue_DequeuedElementMustBeReturnedAndSizeMustBeDecrementedByOne(string element)
+    {
+        // Arrange
+        _queue.Enqueue(element);
+        int beforeSize = _queue.Size;
+
+        // Action
+        string dequeuedElement = _queue.Dequeue();
+
+        // Assertion
+        Assert.Equal(element, dequeuedElement);
+        Assert.Equal(beforeSize - 1, _queue.Size);
+    }
 }
--- Katas.Queue/CustomQueue.cs
+++ Katas.Queue/CustomQueue.cs
@@ -2,5 +2,48 @@
 
 public class CustomQueue<T>
 {
-    
+    private T[] _elements;
+    public bool IsEmpty => Size == 0;
+    public int Size { get; private set; }
+
+    public CustomQueue()
+    {
+        _elements = Array.Empty<T>();
+    }
+
+
+    public void Enqueue(T element)
+    {
+        ++Size;
+
+        T[] newElements = new T[Size];
+
+        for (int i = 0; i < Size; i++)
+        {
+            if (i != Size - 1)
+                newElements[i] = _elements[i];
+        }
+
+        newElements[Size - 1] = element;
+
+        _elements = newElements;
+    }
+
+    public T Dequeue()
+    {
+        --Size;
+
+        T dequeuedElement = _elements[0];
+
+        T[] newElements = new T[Size];
+
+        for (int i = 1; i < Size; i++)
+        {
+            newElements[i] = _elements[i];
+        }
+
+        _elements = newElements;
+
+        return dequeuedElement;
+    }
 }
