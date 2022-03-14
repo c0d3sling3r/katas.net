1133ee7813f4aea57a1ec6dc4422315dcffe3fa6
Mojtaba Shojajou <shojajou@gmail.com>
2022-03-14 17:22:33 +0330

    add Parity project, ParityHelper and its tests

---
+++ Katas.Parity.Tests/TestRunner.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Katas.Parity.Tests;
+
+public class TestRunner
+{
+    [Theory]
+    [InlineData("even", 0)]
+    [InlineData("even", 0, -1, -5)]
+    [InlineData("odd", 2, 5, 34, 6)]
+    [InlineData("odd", 3, 2, 8)]
+    public void PassMultipleNumbers_ParityMustBeCalculatedCorrectly(string expectedParity, params int[] numbers)
+    {
+        // Arrange
+
+        // Action
+        string actualParity = ParityHelper.DetermineArraySum(numbers);
+
+        // Assertion
+        Assert.Equal(expectedParity, actualParity);
+    }
+}
---
+++ Katas.Parity/ParityHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Katas.Parity;
+
+public class ParityHelper
+{
+    public static string DetermineArraySum(int[] numbers)
+    {
+        int sum = numbers.Sum();
+
+        return _getParityString(sum);
+    }
+
+    private static string _getParityString(int number)
+    {
+        return number % 2 == 0 ? "even" : "odd";
+    }
+}
