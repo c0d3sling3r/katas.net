fbc209742d983d3fd1e6f571a8d13cc7caf5cd64
Mojtaba Shojajou <shojajou@gmail.com>
2022-05-25 18:10:40 +0430

    add quick sort logics and tests

---
+++ QuickSort/Katas.QuickSort/Exceptions/UnsortableRangeException.cs
@@ -0,0 +1,8 @@
+﻿namespace Katas.QuickSort.Exceptions;
+
+public class UnsortableRangeException : Exception
+{
+    public UnsortableRangeException() : base("The target range's length must be 2 or more.")
+    {
+    }
+}
